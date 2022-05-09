e6a41fed678520941c22a0a22f3f6da621f36fdf
pnandez <paquiherca@gmail.com>
2022-05-09 11:31:37 +0100

    Co-authored-by: Pablo Bande Sánchez - Girón <alu0101225296@users.noreply.github.com> Co-authored-by: Daniel Álvarez Medina <danielalvarezm09@gmail.com>

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,20 @@
-function sayHelloWorld() {
-  return 'Hello World';
+const axios = require('axios').default;
+class WordChain {
+  public async getNextWord(previousWord: string): string {
+    this.checkIfWordExists(previousWord);
+    return '';
+  }
+
+  private async checkIfWordExists(word: string): Promise<Boolean> {
+    // try {
+    //   const result = Promise.resolve(axios.get(`https://api.dictionaryapi.dev/api/v2/entries/en/${word}`));
+    //   console.log(result.);
+    // } catch (error) {
+    //   console.log(error);
+    // }
+    
+    return true;
+  }
 }
 
-export {sayHelloWorld};
+export { WordChain };
--- test/main.spec.ts
+++ test/main.spec.ts
@@ -1,7 +1,27 @@
-import {sayHelloWorld} from '../src/main';
+import {WordChain} from '../src/main';
 
-describe('Basic test should', () => {
-  it('say "Hello World"', () => {
-    expect(sayHelloWorld() ).toBe('Hello World');
+const countDifferentLettersFromTwoWords = (word1: string, word2: string) => {
+  const word1AsArray = word1.split('');
+  const word2AsArray = word2.split('');
+  let counter = 0;
+  for (let i = 0; i < word1AsArray.length; i++) {
+    if (word1AsArray[i] !== word2AsArray[i]) {
+      counter += 1;
+    }
+  }
+  return counter;
+};
+
+
+describe('Word chain should', () => {
+  it('give a word with only one different letter', () => {
+    const wc = new WordChain();
+    const wordGiven = 'gold';
+
+    const result = wc.getNextWord(wordGiven);
+
+    expect(countDifferentLettersFromTwoWords(wordGiven, result)).toBe(1);
   });
 });
+
+
